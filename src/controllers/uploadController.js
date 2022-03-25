c88eb995898beae9020f1ea5c9d6e675ab786324
Dirac Murairi <diracmurairi@Diracs-MacBook-Pro.local>
2022-03-25 19:38:16 +0200

    fixed db issue

--- src/controllers/uploadController.js
+++ src/controllers/uploadController.js
@@ -1,17 +1,19 @@
 const db = require('../database/models/');
 const dotenv = require('dotenv');
 const aws = require('aws-sdk');
+const v4 = require('uuid').v4;
 var multer = require('multer');
 var multerS3 = require('multer-s3');
 dotenv.config();
 
 const { File } = db;
 
-const bucketName = 'dirac-fiona-achille-upload-files';
+const bucketName = 'dirac-fiona-achille-upload-files-v2';
 
 const s3 = new aws.S3({
   accessKeyId: process.env.AWS_ACCESS_ID,
   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  signatureVersion: 'v4',
 });
 
 // Upload file to S3
@@ -23,22 +25,30 @@ const upload = multer({
       cb(null, { fieldName: file.fieldname });
     },
     key: function (req, file, cb) {
-      cb(null, Date.now().toString());
+      cb(null, `file_${v4()}`);
     },
   }),
 });
 
 const send2db = (filename, link) => {
-  File.create({
+  const file = File.build({
     fileName: filename,
     fileLink: link,
   });
+
+  console.log(file);
+
+  file.save()
 };
 
-const getFiles = async() => {
-  const data = await File.FindAll();
-
-  return data;
+const getFiles = async(req, res) => {
+  try {
+    const data = await File.findAll();
+    res.status(200).json(data);
+  }catch (err) {
+    console.log(err);
+    res.status(500).end();
+  }
 };
 
 module.exports = { upload, send2db, getFiles };
--- src/routes/index.js
+++ src/routes/index.js
@@ -7,10 +7,17 @@ const uploadController = require('../controllers/uploadController');
 
 router.get('/', (_, res) => res.send('Welcome to S3 File Uploader'));
 
-router.post('/upload', uploadController.upload.single('file'), function (req, res, next) {
-    console.log(req.file);
-    uploadController.send2db(req.file.filename,req.file.location )
-    res.send('Uploaded the file to the s3 bucket!');
+router.post('/upload', uploadController.upload.single('myFile'), function (req, res, next) {
+    try {
+        uploadController.send2db(req.file.originalname, req.file.location );
+        const status = {
+            uploadToS3: true,
+            uploadToDb: true,
+        }
+        res.status(200).json(status);
+    } catch(err) {
+        res.status(500).end();
+    }
 });
 
 router.get('/files/', uploadController.getFiles);
